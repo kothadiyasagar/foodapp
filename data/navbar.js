a37b0b6f18d5d229892989b8c700bad8a4cc7cc7
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-03-20 18:42:00 +0530

    d

---
+++ data/navbar.js
@@ -0,0 +1,12 @@
+const navbar = () => {
+    return `<div id="navbar">
+    <h3><a href="index.html">Home</a></h3>
+
+    <div id="options">
+      <h3><a href="get.html">get receipe of the day</a></h3>
+      <h3><a href="show.html">show random receipes</a></h3>
+    
+    </div>
+  </div>`
+};
+export default navbar
